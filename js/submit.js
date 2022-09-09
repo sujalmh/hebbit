2f11e141ed24c3fd802c772e3d672fbedbccd968
sujalmh <sujalmh9@gmail.com>
2022-09-09 21:01:08 +0530

    forms

--- js/submit.js
+++ js/submit.js
@@ -1,60 +1,26 @@
-var basicTimeline = anime.timeline({
-  autoplay: false
-});
+var verified_status =0;
 
-var pathEls = $(".check");
-for (var i = 0; i < pathEls.length; i++) {
-  var pathEl = pathEls[i];
-  var offset = anime.setDashoffset(pathEl);
-  pathEl.setAttribute("stroke-dashoffset", offset);
+let keys = document.querySelectorAll('input');
+
+const input_fields = {
+  username: /^[a-z\d]{5,12}$/i,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  telephone:/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/,
 }
 
-basicTimeline
-  .add({
-    targets: ".text",
-    duration: 1,
-    opacity: "0"
-  })
-  .add({
-    targets: ".button",
-    duration: 1300,
-    height: 10,
-    width: 300,
-    backgroundColor: "#2B2D2F",
-    border: "0",
-    borderRadius: 100
-  })
-  .add({
-    targets: ".progress-bar",
-    duration: 2000,
-    width: 300,
-    easing: "linear"
-  })
-  .add({
-    targets: ".button",
-    width: 0,
-    duration: 1
-  })
-  .add({
-    targets: ".progress-bar",
-    width: 80,
-    height: 80,
-    delay: 500,
-    duration: 750,
-    borderRadius: 80,
-    backgroundColor: "#71DFBE"
-  })
-  .add({
-    targets: pathEl,
-    strokeDashoffset: [offset, 0],
-    duration: 200,
-    easing: "easeInOutSine"
-  });
+const validate = (field, regex) => {
+  regex.test(field.value) ? field.className = 'is-valid' : field.className = 'is-invalid';
+}
 
-$(".button").click(function() {
-  basicTimeline.play();
+
+$('#submitButton').click(function() {
+  element.classList.add("clicked");
+  element.classList.remove("checkmark");
 });
 
-$(".text").click(function() {
-  basicTimeline.play();
-});
+keys.forEach(item => item.addEventListener(
+  'keyup', e => {
+    validate(e.target, input_fields[e.target.attributes.name.value])
+  }
+));
+
